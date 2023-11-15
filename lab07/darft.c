53106f212b1f2e12aeb597a1cf533fb5bf174297
kevin <kevin@Ubuntu18.myguest.virtualbox.org>
2023-11-15 23:39:31 +0800

    finished lab08

---
+++ lab07/darft.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+#include "transpose.h"
+
+int main(){
+
+    srand48( time( NULL ) );
+
+    int x = 0;
+
+    printf("x is: %d", x);
+
+}
